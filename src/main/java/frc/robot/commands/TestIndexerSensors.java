b744c4e1fd4e00566035a6bf57e0a06cdd615eb2
Mochimochimochimochimochi <elizabethcaporiccio@gmail.com>
2022-02-05 14:05:00 -0500

    me when the indexer

--- src/main/java/frc/robot/commands/TestIndexerSensors.java
+++ src/main/java/frc/robot/commands/TestIndexerSensors.java
@@ -15,6 +15,8 @@ public class TestIndexerSensors extends CommandBase {
 
     boolean TOF_Exit; // time of flight sensor at the exit
 
+    boolean TOF_Ent; // time of flight sensor at the entrance
+
     public TestIndexerSensors(IndexerProto subsystem) {
         sys_indexerProto = subsystem;
         addRequirements(subsystem);
@@ -29,17 +31,19 @@ public class TestIndexerSensors extends CommandBase {
         m_colourSensor_etr = sys_indexerProto.getEntranceColour();
         // allianceColour = sys_indexerProto.getFMS();
         TOF_Exit = sys_indexerProto.ballDetectionExit();
+        TOF_Ent = sys_indexerProto.ballDetectionEnter();
 
-        if (m_colourSensor_etr == 'B' || m_colourSensor_etr == 'R' && TOF_Exit == false) {
+        if (m_colourSensor_etr == 'B' || m_colourSensor_etr == 'R' && TOF_Ent == false) {
             sys_indexerProto.spinIndexer(0.5);
             countBalls++;
-        } else if (m_colourSensor_etr == 'B' || m_colourSensor_etr == 'R' && TOF_Exit == true) {
+        } else if (m_colourSensor_etr == 'B' || m_colourSensor_etr == 'R' && TOF_Ent == true) {
             sys_indexerProto.spinIndexer(0);
-            sys_indexerProto.spinPreshooter(0.5);
             countBalls++;
+        } else if(TOF_Exit){
+            sys_indexerProto.spinIndexer(0);
         }
 
-        if (m_colourSensor_etr != 'B' || m_colourSensor_etr == 'R' && TOF_Exit == false)
+        if (m_colourSensor_etr != 'B' || m_colourSensor_etr == 'R' && TOF_Exit == false && TOF_Ent == false)
             countBalls = 0;
 
         System.out.println(countBalls);
