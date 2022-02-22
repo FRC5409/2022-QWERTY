323299fb8e23d760829419635bd66fb9a0a1b5a3
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-02-22 17:00:42 -0500

    added doc

--- src/main/java/frc/robot/base/StateCommand.java
+++ src/main/java/frc/robot/base/StateCommand.java
@@ -5,13 +5,29 @@ import org.jetbrains.annotations.Nullable;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
+/**
+ * <h2> StateCommand </h2>
+ * A state machine representing a complete action to be performed by the robot. Commands are run by
+ * the {@link CommandScheduler}, and can be composed into CommandGroups to allow users to build
+ * complicated multi-step actions without the need to roll the state machine logic themselves.
+ *
+ * <p>Commands are run synchronously from the main robot loop; no multithreading is used, unless
+ * specified explicitly from the command implementation.
+ *
+ * <p>This class is provided by the NewCommands VendorDep
+ *
+ * 
+ * @author Keith Davies
+ */
 public interface StateCommand extends Command {
-    @Nullable
-    String next();
+    void next(String stateName);
+
+    void reset();
 
     @Nullable
-    String next(@Nullable String stateName);
+    String getNextState();
     
     @NotNull
     String getStateName();
+    
 }
--- src/main/java/frc/robot/base/StateCommandGroup.java
+++ src/main/java/frc/robot/base/StateCommandGroup.java
@@ -9,26 +9,52 @@ import org.jetbrains.annotations.Nullable;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * <h2> StateCommandGroup </h2>
+ * A command-based state machine representing a series of tasks to be performed by the robot
+ * in an independant execution order.
+ * 
+ * <p> {@link edu.wpi.first.wpilibj2.command.Command StateCommand}s are an extension of
+ * {@link edu.wpi.first.wpilibj2.command.Command Command}s that represent a task to be
+ * performed by the robot, as opposed to a complete action performed by the robot. This provides
+ * States with the flexibility of Commands, and the portability and seperation
+ * of individual States.
+ * 
+ * @author Keith Davies
+ * @see StateCommand
+ */
 public abstract class StateCommandGroup extends CommandBase {
     protected Map<String, StateCommand> m_states;
     protected StateCommand m_default;
     protected StateCommand m_active;
 
-    protected static Set<StateCommand> getUngrouped(StateCommand... commands) {
-        return Set.of(commands);
-    }
-
+    /**
+     * Construct an empty {@link StateCommandGroup}.
+     */
     public StateCommandGroup() {
         m_active = null;
         m_default = null;
         m_states = new HashMap<>();
     }
     
+    /**
+     * Construct a {@link StateCommandGroup} with several states.
+     * 
+     * @param commands The states included in the group.
+     */
     public StateCommandGroup(StateCommand... commands) {
         this();
         addCommands(commands);
     }
 
+    
+    /**
+     * Construct a {@link StateCommandGroup} with several states,
+     * as well as a default state.
+     * 
+     * @param commands         The states included in the group.
+     * @param defaultStateName The default state name.
+     */
     public StateCommandGroup(String defaultStateName, StateCommand... commands) {
         this();
         addCommands(commands);
@@ -61,11 +87,13 @@ public abstract class StateCommandGroup extends CommandBase {
         if (m_active.isFinished()) {
             m_active.end(false);
 
-            // 'pop' next state name off of state
-            String next = m_active.next(null);
+            String next = m_active.getNextState();
+            m_active.reset();
+
             if (next != null) {
-                System.out.println("Moving to state " + next);
                 m_active = getCommand(next);
+                System.out.println("Moving to state " + next);
+
                 m_active.initialize();
             } else 
                 m_active = null;
@@ -82,8 +110,13 @@ public abstract class StateCommandGroup extends CommandBase {
         m_active = null;
     }
 
+    /**
+     * Add states to group.
+     * 
+     * @param commands The states to add.
+     */
     public void addCommands(StateCommand... commands) {
-        for (StateCommand cmd : getUngrouped(commands)) {
+        for (StateCommand cmd : Set.of(commands)) {
             final String name = cmd.getStateName();
             if (m_states.containsKey(name)) {
                 throw new IllegalArgumentException(
@@ -97,10 +130,34 @@ public abstract class StateCommandGroup extends CommandBase {
         }
     }
 
-    public void setDefaultState(String name) {
+    /**
+     * Set the default state.
+     * 
+     * <p> The default state is the state that runs
+     * when no active state is specified upon initialization
+     * of the command.
+     * 
+     * <p> Under normal circumstances, the default state
+     * acts like the initial state of a state machine.
+     * 
+     * @param name The name of the default state.
+     * 
+     * @throws UnknownStateException If no command with {@code name} exists. 
+     */
+    public void setDefaultState(String name) throws UnknownStateException {
         m_default = getCommand(name);
     }
     
+    /**
+     * Set the active state.
+     * 
+     * <p> The active state is the state that runs
+     * while the state machine is executing.
+     * 
+     * @param name The name of the default state.
+     * 
+     * @throws UnknownStateException If no command with {@code name} exists. 
+     */
     public boolean setActiveState(String name) {
         StateCommand command = getCommand(name);
 
